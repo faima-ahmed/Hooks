46259983f8a2215ee1b8411aa01a7a99ce4a2112
faima-ahmed <faimaahmeddiya@gmail.com>
2024-10-24 10:11:11 +0600

    Commit 1

--- src/components/Hooks/useEffect/UseEffectExample.js
+++ src/components/Hooks/useEffect/UseEffectExample.js
@@ -3,7 +3,7 @@ import React from 'react';
 function UseEffectExample(props) {
     return (
         <div>
-            <h1>UseEffect</h1>
+            <h1>UseEffect</h1>a
         </div>
     );
 }
