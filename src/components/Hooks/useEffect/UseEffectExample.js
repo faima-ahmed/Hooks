6863d0c8ec97a18859eb20955b53ad591c59299e
faima-ahmed <faimaahmeddiya@gmail.com>
2024-10-27 06:14:31 +0600

    Commit 1

--- src/components/Hooks/useEffect/UseEffectExample.js
+++ src/components/Hooks/useEffect/UseEffectExample.js
@@ -1,15 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function UseEffectExample(props) {
     const [count, setCount]= useState(0);
   
-const handleOnClick =()=>{
-    setCount((count)=> count+1);
-}
+// const handleOnClick =()=>{
+//     setCount((count)=> count+1);
+// }
+
+useEffect(() =>{
+    console.log('useEffect');
+})
     return (
         <div>
+            {conso}
             <h1>Count:{count}</h1>
-            <button onClick={handleOnClick}>+</button>
+            <button onClick={()=>{setCount((count)=> count+1)}}>+</button>
         </div>
     );
 }
