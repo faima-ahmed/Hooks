2d6bf6158690deb3d1397bd3649021f0e9b1439a
faima-ahmed <faimaahmeddiya@gmail.com>
2024-11-10 12:19:09 +0600

    Commit 1

--- src/components/Hooks/useEffect/DataFetch.js
+++ src/components/Hooks/useEffect/DataFetch.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function DataFetch(props) {
+    const [todos, setTodos] = useState(null);
+
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/todos');
+    })
     return (
         <div>
             <h1>DataFetch</h1>
