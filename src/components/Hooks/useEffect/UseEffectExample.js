91fd99ced3bed0618374dc862d763147284aefdc
faima-ahmed <faimaahmeddiya@gmail.com>
2024-11-13 00:27:05 +0600

    use-effect

--- src/components/Hooks/useEffect/UseEffectExample.js
+++ src/components/Hooks/useEffect/UseEffectExample.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 function UseEffectExample(props) {
     const [count, setCount]= useState(0);
+    const [isLoading, setIsLoading]= useState(false);
   
 // const handleOnClick =()=>{
 //     setCount((count)=> count+1);
@@ -9,12 +10,13 @@ function UseEffectExample(props) {
 
 useEffect(() => {
     console.log('useEffect');
-},[]);
+},[count]);
     return (
         <div>
             {console.log('rendering')}
             <h1>Count:{count}</h1>
             <button onClick={()=>{setCount((count)=> count+1)}}>+</button>
+            <button onClick={()=>{setIsLoading(!isLoading)}}>isLoading</button>
         </div>
     );
 }
