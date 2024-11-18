8d6ef2609911cf3f5ecf9a1b298015ca020f828f
faima-ahmed <faimaahmeddiya@gmail.com>
2024-11-19 02:12:29 +0600

    Commit 1

--- src/components/Hooks/CustomHooks/DataFetch.js
+++ src/components/Hooks/CustomHooks/DataFetch.js
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react';
 
-const loadingMessage= <p>todos is loading</p>
-
 function DataFetch(props) {
     const [todos, setTodos] = useState(null);
     const [isloading, setIsLoading]=useState(true);
@@ -28,13 +26,16 @@ function DataFetch(props) {
         })
     }, [])
 
+    const loadingMessage= <p>todos is loading</p>
+    const errorMessage= <p>{error}</p>
+
     const todosElement=todos && todos.map((todo)=>{
         return <p key={todo.id}>{todo.title}</p>
      })
     return (
         <div>
             <h1>Todos</h1>
-            {error && <p>{error}</p>}
+            {error && errorMessage}
             {isloading && loadingMessage}
             {todosElement}
         </div>
