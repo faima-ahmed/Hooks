0f64bea00293932c27e811bb5e79d47760448934
faima-ahmed <faimaahmeddiya@gmail.com>
2024-11-20 18:23:10 +0600

    update-code

--- src/components/UseForm.js
+++ src/components/UseForm.js
@@ -2,14 +2,24 @@ import React, { useEffect, useRef } from 'react';
 
 function UseForm(props) {
 const inputRef= useRef(null);
+const headerRef= useRef(null);
 
     useEffect(() =>{
-       inputRef.current.focus();
+       headerRef.current.style.color='purple'
     }, [])
+    
+    let handleInput=()=>{
+        inputRef.current.focus();
+        inputRef.current.value='1000';
+        inputRef.current.style.background='gray'
+
+    }
     return (
         <div>
+            <h1 ref={headerRef}>Learn useRef</h1>
            <p>
             <input ref={inputRef} type='text' placeholder='enter something'/>
+            <button onClick={handleInput}>Submit</button>
             </p> 
         </div>
     );
