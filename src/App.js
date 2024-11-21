a781b461f5c55cdade650ef989ceb4e92dfe27aa
faima-ahmed <faimaahmeddiya@gmail.com>
2024-11-21 17:14:06 +0600

    data-update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import DataFetch from './components/Hooks/CustomHooks/DataFetch';
+import { useState } from "react";
+import Title from "./components/Title";
 
 export default function App() {
+  const [count1, setCount1]= useState(0);
+  const[ count2, setCount2]= useState(0);
+
+  const incrementByOne= () =>{
+    setCount1((prevCount) => prevCount+1);
+  };
+
+  const incrementByFive = ()=>{
+    setCount2((prevCount) => prevCount+5);
+  };
+
   return (
     <div className='App'>
-      <div>
-       <DataFetch/>
-      </div>
+      <Title/>
+      <ShowC
     </div>
   );
 }
---
+++ src/components/Title.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Title = () => {
+    console.log('rendering title...');
+    return (
+        <h2>useCallback Hook Tutorial</h2>
+    );
+};
+
+export default Title;
