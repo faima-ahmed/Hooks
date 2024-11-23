2796c8d7f61ebea62f2bfa66822c17a20897197f
faima-ahmed <faimaahmeddiya@gmail.com>
2024-11-23 22:10:21 +0600

    update-value

--- src/components/Hooks/UseReducer/index.js
+++ src/components/Hooks/UseReducer/index.js
@@ -1,9 +1,20 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+  
+const booksData=[
+    {id:1, name:'Pather panchal'},
+    {id:2, name:'Padma Nadir Majhi'},
+    {id:3, name:'srikanta'},
+]
 const UseReducer = () => {
+    const [books, setBooks]= useState(booksData);
+
     return (
         <div>
-            index
+           <h1>Book List</h1>
+           {books.map((book)=>{
+            const{id, name}=book;
+            return <li key={id}>{name}</li>
+           })}
         </div>
     );
 };
