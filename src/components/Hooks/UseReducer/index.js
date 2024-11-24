c62c73610cb3680bded38c70ada8579e53d332e5
faima-ahmed <faimaahmeddiya@gmail.com>
2024-11-24 17:51:34 +0600

    add-code

--- src/components/Hooks/UseReducer/index.js
+++ src/components/Hooks/UseReducer/index.js
@@ -1,25 +1,38 @@
-import React, { useState } from 'react';
-  
-const booksData=[
-    {id:1, name:'Pather panchal'},
-    {id:2, name:'Padma Nadir Majhi'},
-    {id:3, name:'srikanta'},
-]
+import React, { useState } from "react";
+
+const booksData = [
+  { id: 1, name: "Pather panchal" },
+  { id: 2, name: "Padma Nadir Majhi" },
+  { id: 3, name: "srikanta" },
+];
 const UseReducer = () => {
-    const [books, setBooks]= useState(booksData);
-    const [bookName, setBookName] = useState('');
+  const [books, setBooks] = useState(booksData);
+  const [bookName, setBookName] = useState("");
 
+ const handleSubmit=(e)=>{
+   e.preventDefault();
+   alert(bookName);
+  }
+  return (
+    <div>
+        <h1>Book List</h1>
 
-    <fo
-    return (
-        <div>
-           <h1>Book List</h1>
-           {books.map((book)=>{
-            const{id, name}=book;
-            return <li key={id}>{name}</li>
-           })}
-        </div>
-    );
+  <form onSubmit={handleSubmit}>
+    <input
+      type="text"
+      value={bookName}
+      onChange={(e) => {
+          setBookName(e.target.value);
+        }}
+        />
+    <button type="submit">Add Book</button>
+  </form>
+      {books.map((book) => {
+        const { id, name } = book;
+        return <li key={id}>{name}</li>;
+      })}
+    </div>
+  );
 };
 
 export default UseReducer;
