4606fdc5d6a8c11b7a7b004b8627693aeb475c39
faima-ahmed <faimaahmeddiya@gmail.com>
2024-11-25 17:04:56 +0600

    state-reduce

---
+++ src/components/Hooks/UseReducer/Counter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Counter = () => {
+    return (
+        <div>
+            <div>Count-0</div>
+            <button>Increment</button>
+            <button>Decrement</button>
+        </div>
+    );
+};
+
+export default Counter;
--- src/components/Hooks/UseReducer/index.js
+++ src/components/Hooks/UseReducer/index.js
@@ -1,52 +1,52 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
-const booksData = [
-  { id: 1, name: "Pather panchal" },
-  { id: 2, name: "Padma Nadir Majhi" },
-  { id: 3, name: "srikanta" },
-];
+// const booksData = [
+//   { id: 1, name: "Pather panchal" },
+//   { id: 2, name: "Padma Nadir Majhi" },
+//   { id: 3, name: "srikanta" },
+// ];
 
-const Modal=({modalText})=>{
-return <p>{modalText}</p>
-}
-const UseReducer = () => {
-  const [books, setBooks] = useState(booksData);
-  const [isModalOpen, setIsModalOpen]= useState(false);
-  const [modalText, setModalText]= useState('');
-  const [bookName, setBookName] = useState("");
+// const Modal=({modalText})=>{
+// return <p>{modalText}</p>
+// }
+// const UseReducer = () => {
+//   const [books, setBooks] = useState(booksData);
+//   const [isModalOpen, setIsModalOpen]= useState(false);
+//   const [modalText, setModalText]= useState('');
+//   const [bookName, setBookName] = useState("");
 
- const handleSubmit=(e)=>{
-   e.preventDefault();
-   setBooks((prevState)=>{
-    const newBook= {id:new Date().getTime().toString(), name:bookName};
-    return [...prevState, newBook];
-   });
-   setIsModalOpen(true);
-   setModalText('book is added');
-  };
+//  const handleSubmit=(e)=>{
+//    e.preventDefault();
+//    setBooks((prevState)=>{
+//     const newBook= {id:new Date().getTime().toString(), name:bookName};
+//     return [...prevState, newBook];
+//    });
+//    setIsModalOpen(true);
+//    setModalText('book is added');
+//   };
 
-  return (
-    <div>
-        <h1>Book List</h1>
+//   return (
+//     <div>
+//         <h1>Book List</h1>
 
-  <form onSubmit={handleSubmit}>
-    <input
-      type="text"
-      value={bookName}
-      onChange={(e) => {
-          setBookName(e.target.value);
-        }}
-        />
-    <button type="submit">Add Book</button>
-  </form>
+//   <form onSubmit={handleSubmit}>
+//     <input
+//       type="text"
+//       value={bookName}
+//       onChange={(e) => {
+//           setBookName(e.target.value);
+//         }}
+//         />
+//     <button type="submit">Add Book</button>
+//   </form>
 
-  {isModalOpen && <Modal modalText={modalText}/>}
-      {books.map((book) => {
-        const { id, name } = book;
-        return <li key={id}>{name}</li>;
-      })}
-    </div>
-  );
-};
+//   {isModalOpen && <Modal modalText={modalText}/>}
+//       {books.map((book) => {
+//         const { id, name } = book;
+//         return <li key={id}>{name}</li>;
+//       })}
+//     </div>
+//   );
+// };
 
-export default UseReducer;
+// export default UseReducer;
